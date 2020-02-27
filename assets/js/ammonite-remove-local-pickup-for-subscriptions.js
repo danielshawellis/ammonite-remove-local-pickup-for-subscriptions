556949799f01cac436123b475a08bab3a5c57c57
Daniel Ellis <danielshawellis@gmail.com>
2020-02-26 20:43:10 -0600

    add basic script

--- assets/js/ammonite-remove-local-pickup-for-subscriptions.js
+++ assets/js/ammonite-remove-local-pickup-for-subscriptions.js
@@ -1,3 +1,23 @@
+// Run after DOM content is loaded
 jQuery( document ).ready( function() {
+  // Check if this is the cart page and if any of the subscription radio buttons are checked
+  if ( jQuery( 'body' )[0].classList.contains( 'woocommerce-cart' ) && jQuery( '.subscription-option input:checked' ).length != 0 ) {
+    jQuery( '.toggle-default-handling' ).remove();
+  }
 
+  // Check if this is the checkout page and if any of the table items contain the class "subscription-details"
+  if ( jQuery( 'body' )[0].classList.contains( 'woocommerce-checkout' ) && jQuery( '.subscription-details' ).length != 0 ) {
+    // Wait for AJAX shipping method update
+    jQuery( document.body ).on( 'updated_checkout', function() {
+      jQuery( '.toggle-default-handling' ).remove();
+    } );
+  }
 } );
+
+// Run after cart totals are updated
+jQuery( document.body ).on( 'updated_cart_totals', function() {
+  // Check if this is the cart page and if any of the subscription radio buttons are checked
+  if ( jQuery( 'body' )[0].classList.contains( 'woocommerce-cart' ) && jQuery( '.subscription-option input:checked' ).length != 0 ) {
+    jQuery( '.toggle-default-handling' ).remove();
+  }
+});
